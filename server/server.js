290cfe09cfabd7e4a0f854f344eb00674c242cab
vigneshpalani3 <143024206+vigneshpalani3@users.noreply.github.com>
2024-09-08 13:02:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const corsOptions={
         if(allowedOrigins.indexOf(origin)!==-1 || !origin){
             callback(null,true);
         }else{
-            calllback(new Error("not allowed by CORS"))
+            callback(new Error("not allowed by CORS"))
         }
     }
 }
